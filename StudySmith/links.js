860a76c75cd8a06a5a3ce4fed07a6de085992df5
muneebverse <itsmuneeb825@gmail.com>
2026-02-20 21:48:22 +0500

    Delete StudySmith/links.js

--- StudySmith/links.js
+++
@@ -1,5 +0,0 @@
-const CONTACT = {
-  whatsapp: "https://wa.me/923264213612",
-  instagram: "https://www.instagram.com/studysmithservices?igsh=MXRmOGMyZHU3cTl6Nw==",
-  tiktok: "https://www.tiktok.com/@studysmithservices?is_from_webapp=1&sender_device=pc"
-};
